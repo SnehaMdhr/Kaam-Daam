715a609bd72df602f40770eb84331ca53275d8aa
apala-1 <apalalamichhane13579@gmail.com>
2025-07-02 18:04:39 +0545

    Apala Initial Commit

---
+++ MainFolder/Backend/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const PORT = 3001;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Kaam Damm backend is live!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
