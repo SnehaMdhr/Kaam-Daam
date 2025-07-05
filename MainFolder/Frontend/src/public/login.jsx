2164d800e4d377559bb439c837306a2b8240f8a1
apala-1 <apalalamichhane13579@gmail.com>
2025-07-05 09:29:51 +0545

    Login Backend To Frontend connection sucessful

--- MainFolder/Frontend/src/public/login.jsx
+++ MainFolder/Frontend/src/public/login.jsx
@@ -1,10 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
 import { FaGoogle, FaFacebook, FaTwitter } from "react-icons/fa";
 import logo from "../assets/image/logo.png";
 import bannerImage from "../assets/image/bannerImage.png";
 
 const Login = () => {
+
+  const[email, setEmail] = useState('');
+    const[password, setPassword] = useState('');
+
+  const handleSubmit = async(e) => {
+    e.preventDefault();
+
+  const response = await fetch("http://localhost:5000/api/auth/login", {
+      method: "POST",
+      headers: {
+      "Content-Type": "application/json"
+      },
+      body: JSON.stringify({email, password})
+    });
+
+    const  data = await response.json();
+    console.log(data); 
+
+    localStorage.setItem("token", data.token);
+    if(data.token){
+      console.log("Login Sucessful");
+      window.location.href = "/dashboard";
+    } else {
+      console.error("Login Failed");
+      alert("Login Failed: " + data.message);
+    }
+
+  }
   return (
     <div className="container">
       <div className="left">
@@ -18,12 +46,12 @@ const Login = () => {
         <h2>Welcome Back!</h2>
         <p className="subtitle">Please enter your detail</p>
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <label>Email:</label>
-          <input type="email" placeholder="Enter your email" required />
+          <input type="email" placeholder="Enter your email" value={email} onChange={(e) => setEmail(e.target.value)} required />
 
           <label>Password:</label>
-          <input type="password" placeholder="Enter your password" required />
+          <input type="password" placeholder="Enter your password" value={password} onChange={(e) => setPassword(e.target.value)} required />
 
           <div className="forgot">
             <a href="#">Forget Password</a>
