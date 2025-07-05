a14f503443804d0b9a198190f3ab1b56591f18ab
apala-1 <apalalamichhane13579@gmail.com>
2025-07-05 22:29:47 +0545

    Google Login Sucessful

--- MainFolder/Frontend/src/App.jsx
+++ MainFolder/Frontend/src/App.jsx
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 const Login = lazy(() => import("./public/login"));
 const Register = lazy(() => import("./public/register"));
 const ResetPassword = lazy(() => import("./public/forget"));
+const RoleSelection = lazy(() => import("./public/RoleSelection")); 
+const GoogleRedirect = lazy(() => import("./public/GoogleRedirect"));
 
 function App() {
   return (
@@ -13,7 +15,9 @@ function App() {
           <Route path="/" element={<Login />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/forget" element={<ResetPassword />} />x
+          <Route path="/forget" element={<ResetPassword />} />
+          <Route path="/role-selection" element={<RoleSelection />} />
+          <Route path="/google-redirect" element={<GoogleRedirect />} />
         </Routes>
       </Suspense>
     </Router>
