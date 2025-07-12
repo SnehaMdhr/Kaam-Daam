b7942abd18222c992670b5f83417ba80c474820e
anjaliofficial <bistaanjali1415@gmail.com>
2025-07-12 12:19:47 +0545

    New Profile Page created

--- MainFolder/Frontend/src/public/studentprofile.jsx
+++ MainFolder/Frontend/src/public/studentprofile.jsx
@@ -1,6 +1,6 @@
 import { React, useState, useEffect } from "react";
 import axios from "axios";
-import Header from "../components/headerforemployer";
+import Header from "../components/headerforstudent";
 import Sidebar from "../components/sidebarstudent";
 import "./studentprofile.css";
 
