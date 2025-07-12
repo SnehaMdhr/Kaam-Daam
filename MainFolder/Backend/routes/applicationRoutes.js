228744a7a4793b2e69f11ffa2064602eb3c736e9
apala-1 <apalalamichhane13579@gmail.com>
2025-07-12 17:07:52 +0545

    ViewProfile

--- MainFolder/Backend/routes/applicationRoutes.js
+++ MainFolder/Backend/routes/applicationRoutes.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const { applyToJob, getApplicationsByEmployer, getApplicationsByStudent, checkAlreadyApplied } = require('../controllers/applicationController');
+const { applyToJob, getApplicationsByEmployer, getApplicationsByStudent, checkAlreadyApplied, updateApplicationStatus } = require('../controllers/applicationController');
 const { protect } = require('../middleware/authMiddleware');
 const { route } = require('./users');
+
 
 router.post('/apply', protect, applyToJob);
 router.get('/employer', protect, getApplicationsByEmployer);
 router.get('/student', protect, getApplicationsByStudent);
 router.get('/check/:jobId', protect, checkAlreadyApplied);
+router.put('/status/:applicationId', protect, updateApplicationStatus);
 
 module.exports = router;
