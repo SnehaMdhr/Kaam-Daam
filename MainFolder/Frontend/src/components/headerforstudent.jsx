ac06f0d13f7d6bf2dd2b3ce7ad3f2f80337ed6f8
anjaliofficial <bistaanjali1415@gmail.com>
2025-07-12 12:19:47 +0545

    New Profile Page created

---
+++ MainFolder/Frontend/src/components/headerforstudent.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { FaBell, FaUserCircle } from "react-icons/fa";
+import logo from "../assets/image/logo.png";
+import "./headerforstudent.css";
+import { useNavigate } from "react-router-dom";
+
+const HeaderForStudent = () => {
+  const navigate = useNavigate();
+
+  const goToProfile = () => {
+    navigate("/studentviewprofile");
+  };
+
+  return (
+    <nav className="topbar">
+      <div className="container nav-content">
+        <div className="logo">
+          <img src={logo} alt="logo" />
+          <h3>Kaam Daam</h3>
+        </div>
+        <div className="topbar-right">
+          <FaBell className="icon bell-icon" />
+          <FaUserCircle
+            className="icon profile-icon"
+            onClick={goToProfile}
+            style={{ cursor: "pointer" }}
+          />
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default HeaderForStudent;
