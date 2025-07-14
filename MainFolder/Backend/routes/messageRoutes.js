1fec248beebffde8601d5e76474252e25d0f8d0b
anjaliofficial <bistaanjali1415@gmail.com>
2025-07-14 12:39:02 +0545

    Employer Side Messaging sidebar fixed

--- MainFolder/Backend/routes/messageRoutes.js
+++ MainFolder/Backend/routes/messageRoutes.js
@@ -3,12 +3,15 @@ const router = express.Router();
 const {
   sendMessage,
   getMessages,
-  getHiredEmployers
+  getHiredEmployers,
+  getHiredStudents
 } = require('../controllers/messageController');
 const { protect } = require('../middleware/authMiddleware');
 
 // ✅ More specific route goes first
 router.get('/hired-employers/:studentId', protect, getHiredEmployers);
+router.get('/hired-students/:employerId', getHiredStudents);
+
 
 // ✅ Then general route
 router.get('/:senderId/:receiverId', protect, getMessages);
